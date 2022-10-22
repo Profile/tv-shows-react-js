440f61186c3b7aa28fa01d6389034589d9046d02
Aslan Mammadrzayev <aslan@Aslans-MacBook-Pro.local>
2022-10-22 10:33:39 +0400

    feat(header) Add burger menu and increase search-bar width

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback } from 'react';
 import { NavLink, Link, useLocation } from 'react-router-dom';
+
 import { DEFAULT_PATH } from '../../consts';
+import BurgerMenu from '../../assets/svgs/burger.svg';
 
 type TProps = {
   children: React.ReactNode;
@@ -18,34 +20,55 @@ export const MainLayout = ({ children }: TProps) => {
 
   const generatePageName = useCallback((pageName: string) => `/category/${pageName}`, []);
 
+  const handleMenu = () => {
+    console.log('...');
+  };
+
+  const renderMenu = () => {
+    return (
+      <nav className=" h-full">
+        <ul className="flex h-full">
+          {pages.map((item) => (
+            <li key={item}>
+              <NavLink
+                className={`flex items-center h-full capitalize px-3 ${
+                  currentPage === generatePageName(item) ? 'text-[#027aff]' : undefined
+                }`}
+                to={generatePageName(item)}
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                {item}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  };
+
   return (
     <>
-      <div className="h-[50px] fixed w-full border-b-2">
-        {/* burger menu for menu */}
-        <div className="flex h-full">
-          <div className="main-logo flex items-center px-4 justify-center">
-            <Link to={DEFAULT_PATH}>Logo</Link>
+      <div className="h-[50px] fixed w-full flex justify-center border-b-2 px-4">
+        <div className="max-w-[1200px] w-full h-full bg-red">
+          <div className="flex md:hidden items-center w-[30px]">
+            <img
+              onClick={handleMenu}
+              className="cursor-pointer w-full"
+              src={BurgerMenu}
+              alt="burger-menu"
+            />
           </div>
-          <div>
-            <input type="text" />
+          <div className="flex h-full flex-1 justify-end md:justify-start">
+            <div className="main-logo flex items-center justify-center">
+              <Link to={DEFAULT_PATH}>Logo</Link>
+            </div>
+            <div className="mx-4 hidden md:block"></div>
+            <div className="hidden md:flex items-center max-w-xs w-full">
+              <input type="search" className="bg-[#efefef] h-[36px] rounded-md p-3 w-full" />
+            </div>
+            <div className="mx-4 hidden md:block"></div>
+            <div className="hidden md:block">{renderMenu()}</div>
           </div>
-          <nav className=" h-full">
-            <ul className="flex h-full">
-              {pages.map((item) => (
-                <li key={item}>
-                  <NavLink
-                    className={`flex items-center h-full capitalize px-3 ${
-                      currentPage === generatePageName(item) ? 'text-[#027aff]' : undefined
-                    }`}
-                    to={generatePageName(item)}
-                    style={({ isActive }) => (isActive ? activeStyle : undefined)}
-                  >
-                    {item}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </nav>
         </div>
       </div>
       <div className="h-[50px]"></div>
